9a71e5db8345fd8f334a1d6f621a99ff29bd471b
Oleg <olegglotov0@gmail.com>
2025-02-13 01:27:51 +0200

    Output of a number in a column

--- main2.py
+++ main2.py
@@ -1,1 +1,8 @@
-print("hello hillel21")
+user_number = input("Введіть число тільки чотиризначне: ")
+
+number = int(user_number)
+if 1000 <= number <= 9999:
+ print(number // 1000)
+ print(number // 100 % 10)
+ print(number //10 % 10 )
+ print(number % 10)
