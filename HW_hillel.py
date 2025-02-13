3b4bae2816261dfbdf88e46e96e19359eca56d1a
Oleg <olegglotov0@gmail.com>
2025-02-13 02:05:32 +0200

    It is necessary to "turn over" a 5-digit number

--- HW_hillel.py
+++ HW_hillel.py
@@ -2,5 +2,4 @@ user_number = input("Введіть число тільки чотиризнач
 
 number = int(user_number)
 if 10000 <= number <= 99999:
-
  print(number % 10, number // 10 % 10, number // 100 % 10, number // 1000 % 10, number // 10000)
