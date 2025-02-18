3877ea6826b3de775e056c9da4da795929beaf94
Oleg G <117397777+toyshell@users.noreply.github.com>
2025-02-18 03:34:54 +0200

    Delete main2.py

--- main2.py
+++
@@ -1,8 +0,0 @@
-user_number = input("Введіть число тільки чотиризначне: ")
-
-number = int(user_number)
-if 1000 <= number <= 9999:
- print(number // 1000)
- print(number // 100 % 10)
- print(number //10 % 10 )
- print(number % 10)
