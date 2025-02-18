7d02b670aecc7a03d831d28048fa58e7007c000f
Oleg <olegglotov0@gmail.com>
2025-02-18 03:36:56 +0200

    It is performance of 3 tasks

--- HW_hillel.py
+++ HW_hillel.py
@@ -1,5 +1,49 @@
-user_number = input("Введіть число тільки чотиризначне: ")
 
-number = int(user_number)
-if 10000 <= number <= 99999:
- print(number % 10, number // 10 % 10, number // 100 % 10, number // 1000 % 10, number // 10000)
+
+# HW 3.1
+
+x = int(input("введіть число перщої змінної "))
+y = int(input("введіть число другої змінної "))
+operators = input("виберіть оператора +.-,/,* ")
+
+if operators == "+":
+    print(x+y)
+elif operators == "-":
+    print(x-y)
+elif operators == "*":
+    print(x*y)
+elif operators == "/":
+  if y == 0:
+    print("ви ввели 0, введіть не 0 ")
+  else:
+    print(x / y)
+else:
+
+    print("невірне число")
+
+
+
+
+# HW 3.2
+
+lst = [1]
+
+if len(lst) > 0:
+    last_number = lst.pop()
+    lst.insert(0,last_number)
+print(lst)
+
+
+ # HW 3.3
+
+lst = [1]
+
+if len(lst) == 0:
+    print([[], []])
+elif len(lst) % 2 == 0:
+    number = len(lst) // 2
+    print([lst[:number], lst[number:]])
+else:
+    number = len(lst) // 2 + 1
+    print([lst[:number], lst[number:]])
+
