2c1fd3f734279e0b472a4ef4ce27a47463ad0fc5
Oleg <olegglotov0@gmail.com>
2025-03-10 20:12:25 +0200

    It is performance of 4 tasks

--- HW_hillel.py
+++ HW_hillel.py
@@ -9,7 +9,6 @@ assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
 assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
 print('ОК')
 
-
 # Заавдання 7.2
 
 def correct_sentence(text):
