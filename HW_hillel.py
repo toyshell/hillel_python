634c8261101654fcb1e1655a147e10d9834b36db
Oleg <olegglotov0@gmail.com>
2025-03-11 15:39:18 +0200

    It is performance of 3 tasks

--- HW_hillel.py
+++ HW_hillel.py
@@ -1,3 +1,55 @@
+
+# Завдання              8.1      8.2      8.3
+
+# Завдання 8.1
+
+def add_one(some_list):
+
+   number = int("".join(map(str, some_list)))
+
+   number += 1
+
+   return [int(digit) for digit in str(number)]
+
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+assert add_one([0]) == [1], 'Test3'
+assert add_one([9]) == [1, 0], 'Test4'
+
+print("ОК")
+
+# Завдання 8.2
+
+def is_palindrome(text):
+
+    text_cleaned = "".join(el.lower() for el in text if el.isalnum())
+
+    return text_cleaned == text_cleaned[::-1]
+
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+
+print("ОК")
+
+# Завдання 8.3
+
+def find_unique_value(some_list):
+   for number in some_list:
+      if some_list.count(number) == 1:
+           return number
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+
+print("ОК")
+
+
+
+"""
 # Завдання                   7.1          7.2          7.3         7.4
 
 # Завдання  7.1
@@ -65,7 +117,7 @@ print("ОК")
 
 
 
-"""
+
 #HW Завдання                  6.1         6.2          6.3
 
 # Завдання 6,1
@@ -121,24 +173,6 @@ while numbers >= 9:
 
  numbers = new_number
 print(numbers)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # Завдання              5.1               5.2               5.3
