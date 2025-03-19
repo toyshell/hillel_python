32b1dc3ee2e3f1e042c76f2351476c7cdb5779cf
Oleg <olegglotov0@gmail.com>
2025-03-20 00:13:33 +0200

    It is performance of 3 tasks

--- HW_hillel.py
+++ HW_hillel.py
@@ -1,3 +1,66 @@
+#Завдання  10,1             10,2             10,3
+
+#Завдання 10.1
+
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    """
+     begin: перший елемент послідовності
+     end: кількість елементів у послідовності
+     func: функція, яка формує значення для послідовності
+    """
+    value = begin
+    for el in range(end):
+        yield  value
+        value = func(value)
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
+
+# Завдання 10.2
+
+import re
+
+def first_word(text: str) -> str:
+
+    new_text = re.search(r"[a-zA-Zа-яА-ЯёЇїІіЄєҐґ']+", text)
+    return new_text.group() if new_text else ""
+
+
+assert first_word("Hello world!") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
+
+# Завдання 10.3
+
+def is_even(digit):
+
+    if digit % 2 == 0:
+        return True
+    else:
+        return False
+
+
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')
+
+
+
+"""
+
 # Завдання        9.1            9.2
 
 # Завдання 9.1
@@ -27,7 +90,6 @@ assert difference() == 0, 'Test4'
 print('OK')
 
 
-"""
 # Завдання              8.1      8.2      8.3
 
 # Завдання 8.1
