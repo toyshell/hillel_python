fa3c6f1584e47639cf923756cddaf1b83be05993
Oleg <olegglotov0@gmail.com>
2025-03-27 02:19:34 +0200

    It is performance of 2 tasks

--- HW_hillel.py
+++ HW_hillel.py
@@ -1,3 +1,103 @@
+# Завдання  12.1            12.2
+
+# Завдання 12.1
+
+import codecs
+import re
+
+def delete_html_tags(html_file='draft.html', result_file='cleaned.txt'):
+
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html = file.read()
+
+
+    clean_html = re.sub(r'<[^>]+>', '', html)
+
+
+    with codecs.open(result_file, 'w', 'utf-8') as output_file:
+        output_file.write(clean_html)
+
+delete_html_tags('draft.html', 'cleaned.txt')
+
+
+# Завдання 12.2
+
+class Item:
+
+    def __init__(self, name, price, description, dimensions):
+        self.price = price
+        self.description = description
+        self.dimensions = dimensions
+        self.name = name
+
+    def __str__(self):
+        return f"Price:{self.price},Description:{self.description},Dimensions:{self.dimensions},Name:{self.name}"
+
+class User:
+
+    def __init__(self, name, surname, numberphone):
+        self.name = name
+        self.surname = surname
+        self.numberphone = numberphone
+
+    def __str__(self):
+        return f"User:{self.name} {self.surname},Numberphone:{self.numberphone}"
+
+class Purchase:
+    def __init__(self, user):
+        self.products = {}
+        self.user = user
+        self.total = 0
+
+    def add_item(self, item, cnt):
+        if item in self.products:
+          self.products[item] = +cnt
+        else:
+          self.products[item] = cnt
+
+    def __str__(self):
+        # Створення рядка items_str, що містить інформацію про кожен товар
+        items_str = " ".join([f"{item.name}: {cnt} pcs., price: {item.price}" for item, cnt in self.products.items()])
+
+        # Повертається форматований рядок з інформацією про користувача та товари
+        return f"User: {self.user.name} {self.user.surname} Items: {items_str}"
+
+    def get_total(self):
+        return sum(item.price * cnt for item, cnt in self.products.items())
+
+lemon = Item('lemon', 5, "yellow", "small", )
+apple = Item('apple', 2, "red", "middle", )
+print(lemon)  # lemon, price: 5
+
+buyer = User("Ivan", "Ivanov", "02628162")
+print(buyer)  # Ivan Ivanov
+
+cart = Purchase(buyer)
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
+print(cart)
+
+#User: Ivan Ivanov
+#Items:
+#lemon: 4 pcs.
+#apple: 20 pcs.
+
+assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
+assert cart.get_total() == 60, "Всього 60"
+assert cart.get_total() == 60, 'Повинно залишатися 60!'
+cart.add_item(apple, 10)
+print(cart)
+
+#User: Ivan Ivanov
+#tems:
+#lemon: 4 pcs.
+#apple: 10 pcs.
+
+
+assert cart.get_total() == 40
+print('ok')
+
+
 # Завдання         11.1            11.2               11.3
 
 # Завдання 11.1
@@ -53,7 +153,7 @@ print("Ok")
 
 
 
-"""
+
 #Завдання  10,1             10,2             10,3
 
 #Завдання 10.1
