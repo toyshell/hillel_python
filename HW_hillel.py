f2d1117ee8d1a11c55647ca567f69886907a26e9
Oleg <olegglotov0@gmail.com>
2025-03-31 02:35:18 +0300

    It is performance of 1 tasks

--- HW_hillel.py
+++ HW_hillel.py
@@ -1,3 +1,77 @@
+
+
+# Завдання 14.2
+
+class Human:
+
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'{self.first_name},{self.last_name},{self.age}років,{self.gender}'
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f'{super().__str__()},{self.record_book}'
+
+    def __eq__(self, other):
+        if isinstance(other, Student):
+            return str(self) == str(other)
+        return False
+
+    def __hash__(self):
+        return hash(str(self))
+
+class Group:
+
+    def __init__(self, number):
+        self.number = number
+        self.group = []
+
+    def add_student(self, student):
+        if student not in self.group:
+            self.group.append(student)
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+
+        return f'Number:{self.number}\\n {all_students} '
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+print(gr)
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+print('ok')
+gr.delete_student('Taylor')
+print(gr)  # Only one student
+
+gr.delete_student('Taylor')  # No error!
+
+"""
 # Завдання 13.1     13.2
 
 # Завдання 13.1
@@ -63,6 +137,7 @@ print(gr)  # Only one student
 
 gr.delete_student('Taylor')  # No error!
 
+
 # Завдання 13.2
 
 
@@ -121,8 +196,6 @@ assert counter.get_current() == 7, 'Test4'
 print('OK')
 
 
-
-"""
 # Завдання  12.1            12.2
 
 # Завдання 12.1
